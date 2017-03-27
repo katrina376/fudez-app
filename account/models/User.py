eb2f88a23015caa81cc10a67c5ce1a9029b39428
Katrina Chan <katrina.hyc@gmail.com>
2017-03-28 03:07:46 +0800

    Fix create method; add President to kind choice

--- account/models/User.py
+++ account/models/User.py
@@ -2,56 +2,46 @@ from django.contrib.auth.models import AbstractBaseUser, AbstractBaseUserManager
 from django.db import models
 
 class UserManager(AbstractBaseUserManager):
-    def new(self, username, password, identity, department):
+    def create_user(self, username, password, identity, department):
         user = self.model(
             username=username,
             identity=identity,
             department=department
         )
-        # TODO: What if department_id cannot match department?
         user.set_password(password)
         user.save()
         return user
 
 class User(AbstractBaseUser):
     USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['identity', 'department']
+    REQUIRED_FIELDS = ['name', 'email', 'department', 'kind']
 
-    # Identity Choices
+    # Kind Choices
     D_STAFF = 'S'
     D_CHIEF = 'C'
     F_STAFF = 'A'
     F_CHEIF = 'F'
-    IDENTITY_CHOICES = (
+    PRESIDENT = 'P'
+    KIND_CHOICES = (
         (D_STAFF, '請款部員'),
         (D_CHIEF, '部長'),
         (F_STAFF, '財務部部員'),
         (F_CHEIF, '財務部部長'),
+        (PRESIDENT, '會長')
     )
 
-    username = models.CharField(max_length=16, unique=True)
+    username = models.CharField(max_length=16, primary_key=True)
+    name = models.CharField(max_length=16)
+    email = models.EmailField()
 
     department = models.ForeignKey('account.models.Department')
-    identity = models.CharField(max_length=1, choices=IDENTITY_CHOICES)
+    kind = models.CharField(max_length=1, choices=KIND_CHOICES)
 
-    create_date = models.DateField(auto_now_add=True)
+    create_time = models.DateTimeField(auto_now_add=True)
+    last_modify_time = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(default=True)
 
     objects = UserManager()
-
-    def activate(self):
-        self.is_active = True
-        self.save()
-        return self
-
-    def deactivate(self):
-        self.is_active = False
-        self.save()
-        return self
-
-    def set_department(self, department):
-        self.department = department
-        return self
-
+    
     def __str__(self):
         return self.username
