3dab973421a781ddd954af3a0074a157dd211e5d
Katrina Chan <katrina.hyc@gmail.com>
2017-03-28 16:39:46 +0800

    Add __str__()

--- budget/models/Session.py
+++ budget/models/Session.py
@@ -4,3 +4,6 @@ class Session(models.Model):
     name = models.CharField(max_length=16)
     start_date = models.DateField()
     end_date = models.DateField()
+
+    def __str__(self):
+        return self.name
