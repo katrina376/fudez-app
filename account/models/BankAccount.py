469b4b3db096cd64586307150a9da28a7c3f12c8
Katrina Chan <katrina.hyc@gmail.com>
2017-08-28 16:31:53 +0800

    Add field for bank code

--- account/models/BankAccount.py
+++ account/models/BankAccount.py
@@ -3,7 +3,8 @@ from django.db import models
 class BankAccount(models.Model):
     department = models.ForeignKey('account.Department')
 
-    bank_name = models.CharField(max_length=4)
+    bank_name = models.CharField(max_length=10)
+    bank_code = models.CharField(max_length=4)
     branch_name = models.CharField(max_length=5)
     account = models.CharField(max_length=20)
     account_name = models.CharField(max_length=16)
