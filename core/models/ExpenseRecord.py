81682aef5d271ff1fb561a512893b5efe6bfcf33
Katrina Chan <katrina.hyc@gmail.com>
2017-09-23 03:22:43 +0800

    Rewrite ExpenseRecord fields

--- core/models/ExpenseRecord.py
+++ core/models/ExpenseRecord.py
@@ -1,17 +1,17 @@
 from django.db import models
 
 class ExpenseRecord(models.Model):
-    requirement = models.OneToOneField('core.Requirement', primary_key=True, related_name='expense_record')
+    # Kind Choices
+    INCOME = 'I'
+    EXPENSE = 'E'
+    KIND_CHOICES = (
+        (INCOME, '收入'),
+        (EXPENSE, '支出')
+    )
 
-    serial_number = models.CharField(max_length=8)
+    requirement = models.ForeignKey('core.Requirement', primary_key=True, related_name='expense_records')
+    kind = models.CharField(max_length=1, choices=KIND_CHOICES)
+
     memo = models.TextField()
-    remit_date = models.DateField(null=True)
-    income = models.PositiveIntegerField(default=0)
-    expense = models.PositiveIntegerField(default=0)
-
-    @property
-    def balance(self):
-        return 0
-
-    def __str__(self):
-        return '{} {}'.format(self.remit_date, self.balance)
+    date = models.DateField(null=True)
+    amount = models.PositiveIntegerField(default=0)
