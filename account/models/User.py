08745f49dc922995dc81d399f3d074c33ca47932
Katrina Chan <katrina.hyc@gmail.com>
2017-10-12 04:25:51 +0800

    Adjust some fields and remove self-def UserManager since the original one fits in our case

--- account/models/User.py
+++ account/models/User.py
@@ -1,47 +1,56 @@
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+import uuid
+
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
 from django.db import models
 
-class UserManager(BaseUserManager):
-    def create_user(self, username, password, kind, department):
-        user = self.model(
-            username=username,
-            kind=kind,
-            department=department
-        )
-        user.set_password(password)
-        user.save()
-        return user
+from account.models import Department
 
-class User(AbstractBaseUser):
-    USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['name', 'email', 'department', 'kind']
+def get_sentinel_department():
+    return Department.objects.get_or_create(id=0)[0]
 
+class User(AbstractBaseUser, PermissionsMixin):
     # Kind Choices
     DEPARTMENT = 'D'
     STAFF = 'S'
     CHEIF = 'C'
     PRESIDENT = 'P'
     AUDIT = 'A'
+    ENGINEER = 'E'
     KIND_CHOICES = (
         (DEPARTMENT, '一般單位請款帳號'),
         (STAFF, '財務部部員'),
         (CHEIF, '財務部部長'),
         (PRESIDENT, '會長、議長、院務會議'),
-        (AUDIT, '審查')
+        (AUDIT, '審查'),
+        (ENGINEER, '工程師')
     )
 
-    username = models.CharField(max_length=16, primary_key=True)
+    id = models.UUIDField(default=uuid.uuid4, editable=False)
+    username = models.CharField(max_length=16, primary_key=True, unique=True)
     name = models.CharField(max_length=16)
     email = models.EmailField()
 
-    department = models.ForeignKey('account.Department', related_name='users')
-    kind = models.CharField(max_length=1, choices=KIND_CHOICES)
+    department = models.ForeignKey('account.Department', on_delete=models.SET(get_sentinel_department), related_name='users', null=True)
+    kind = models.CharField(max_length=1, choices=KIND_CHOICES, default=DEPARTMENT)
 
     create_time = models.DateTimeField(auto_now_add=True)
     edit_time = models.DateTimeField(auto_now=True)
+
     is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
 
     objects = UserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['name', 'email', 'kind']
+
+    def get_long_name(self):
+        return self.name
+
+    def get_short_name(self):
+        return self.name
 
     def __str__(self):
         return self.username
