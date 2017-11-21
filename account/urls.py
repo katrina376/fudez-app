55df180d186ec2bf5361443ce3ebf7a41a919cb0
Katrina Chan <katrina.hyc@gmail.com>
2017-11-22 03:03:32 +0800

    Register views to urls

---
+++ account/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url, include
+from rest_framework.routers import DefaultRouter
+from account.views import UserViewSet, DepartmentViewSet, BankAccountViewSet, UnlockRecordViewSet
+from rest_framework_nested import routers
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet, base_name='user')
+router.register(r'departments', DepartmentViewSet, base_name='department')
+
+departments_router = routers.NestedSimpleRouter(router, r'departments', lookup='department')
+departments_router.register(r'accounts', BankAccountViewSet, base_name='department-account')
+departments_router.register(r'records', UnlockRecordViewSet, base_name='department-record')
+
+urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^', include(departments_router.urls)),
+]
