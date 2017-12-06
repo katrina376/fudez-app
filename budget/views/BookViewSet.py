3c95204f04dda8d411a3ea3f26b3aadadc5cad12
Katrina Chan <katrina.hyc@gmail.com>
2017-12-06 21:15:20 +0800

    Add query param for department

--- budget/views/BookViewSet.py
+++ budget/views/BookViewSet.py
@@ -1,8 +1,19 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, permissions
+
+from account.models import Department
 from budget.models import Book
 from budget.serializers import BookSerializer
 
+
 class BookViewSet(viewsets.ModelViewSet):
-    queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = (permissions.IsAuthenticated,)
+
+    def get_queryset(self):
+        if 'department' in self.request.query_params:
+            pk = self.request.query_params['department']
+            departmemt = get_object_or_404(Department.objects.all(), pk=pk)
+            return Book.objects.filter(projects__in=departmemt.projects.all())
+        else:
+            return Book.objects.all()
