c8d27591a173946e3f0e24ad22562489b4b1ddd1
Katrina Chan <katrina.hyc@gmail.com>
2017-12-06 20:54:21 +0800

    Remove nested

--- account/urls.py
+++ account/urls.py
@@ -1,18 +1,14 @@
 from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
 from account.views import UserViewSet, DepartmentViewSet, BankAccountViewSet, UnlockRecordViewSet, MeView
-from rest_framework_nested import routers
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet, base_name='user')
 router.register(r'departments', DepartmentViewSet, base_name='department')
-
-departments_router = routers.NestedSimpleRouter(router, r'departments', lookup='department')
-departments_router.register(r'accounts', BankAccountViewSet, base_name='department-account')
-departments_router.register(r'records', UnlockRecordViewSet, base_name='department-record')
+router.register(r'records', UnlockRecordViewSet, base_name='record')
+router.register(r'departments/(?P<department_pk>.+)/accounts', BankAccountViewSet, base_name='department-account')
 
 urlpatterns = [
     url(r'^users/me', MeView.as_view()),
     url(r'^', include(router.urls)),
-    url(r'^', include(departments_router.urls)),
 ]
