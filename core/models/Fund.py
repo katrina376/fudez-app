46f8cabe36942b2008df1edffb5d4fe8637bee96
Katrina Chan <katrina.hyc@gmail.com>
2017-12-06 21:20:04 +0800

    Adjust the methods and structure for dealing with the determination of kind

--- core/models/Fund.py
+++ core/models/Fund.py
@@ -1,23 +1,34 @@
 from django.db import models
 
 class FundQuerySet(models.QuerySet):
-    def approved(self):
-        return self.filter(status=Fund.NORMAL, requirement__state=Requirement.COMPLETE)
+    def normal(self):
+        return self.filter(status=Fund.NORMAL)
 
-class FundManager(models.Maneger):
+    def approved(self):
+        funds = self.filter(status=Fund.NORMAL, requirement__regularrequirement__isnull=False)
+        pk_list = [fund.pk for fund in funds if fund.requirement.state == fund.requirement.COMPLETE]
+        return self.filter(pk__in=pk_list)
+
+class FundManager(models.Manager):
     def get_queryset(self):
         return FundQuerySet(self.model, using=self._db)
+
+    def normal(self):
+        return self.get_queryset().normal()
 
     def approved(self):
         return self.get_queryset().approved()
 
     def approve_reserves(self, amount, requirement):
-        original_fund = requirement.funds.get(item__subject__is_reserves=True)
-        item = original_fund.item
-        fund = self.create(item=item, requirement=requirement, amount=amount, reserves=original_fund)
-        original_fund.status = Fund.DEPRECATED
-        original_fund.save()
-        return fund
+        if requirement.funds.filter(item__subject__is_reserves=True).exists():
+            original_fund = requirement.funds.get(item__subject__is_reserves=True)
+            item = original_fund.item
+            fund = self.create(item=item, requirement=requirement, amount=amount, reserves=original_fund)
+            original_fund.status = self.model.DEPRECATED
+            original_fund.save()
+            return fund
+        else:
+            return None
 
 class Fund(models.Model):
     # Status Choices
@@ -32,9 +43,11 @@ class Fund(models.Model):
 
     item = models.ForeignKey('budget.Item', on_delete=models.PROTECT, related_name='funds')
     requirement = models.ForeignKey('core.Requirement', on_delete=models.CASCADE, related_name='funds')
-    reserves = models.ForeignKey('core.Fund', on_delete=models.CASCADE, null=True, unique=True)
 
     amount = models.PositiveIntegerField()
     memo = models.TextField()
 
     objects = FundManager()
+
+    def __str__(self):
+        return 'Requirement {} Fund {}'.format(self.requirement.pk, self.id)
