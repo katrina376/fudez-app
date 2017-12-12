ba47f4e072024580bbecbf7a5fbfc86c69f795e8
Katrina Chan <katrina.hyc@gmail.com>
2017-12-13 00:09:49 +0800

    Remove logging middleware

--- main/settings.py
+++ main/settings.py
@@ -39,7 +39,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'utils.middleware.LoggingMiddleware',
     'corsheaders.middleware.CorsMiddleware',
 ]
 
